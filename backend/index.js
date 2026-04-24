ade937c89d9168c176fd5be9f6532be47df9f1f9
Mohammad Asad <asadkha2104@gmail.com>
2026-04-24 18:44:42 +0530

    deployment fix

--- backend/index.js
+++ backend/index.js
@@ -155,4 +155,5 @@ app.post("/bfhl", (req, res) => {
   });
 });
 
-app.listen(3000, () => console.log("Server running on 3000"));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log("Server running"));
